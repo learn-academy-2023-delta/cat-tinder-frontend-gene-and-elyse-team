0318578c138b0bc322a1caf8ea0a6d1cbfbc9a76
Elyse Montano <elysemontano.dev.com>
2023-06-27 14:38:54 -0700

    adds frontend structure and styles for header, footer, and not found pages

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>&copy; 2023 LEARN | Elyse and Gene</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className="page-body">
+        <h1>Oops! Cat knocked this page off the table. </h1>
+        <img className="cat-friend" src="https://www.ovrs.com/blog/wp-content/uploads/2020/02/OVRS-Cats-knock-over-shutterstock_1484418140-2000x1977.jpg" alt="Cat knocked the page over, error."/>
+    </div>
+  )
+}
+
+export default NotFound
