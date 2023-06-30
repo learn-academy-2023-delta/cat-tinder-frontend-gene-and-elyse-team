0518aac8164fd903d6ba68459a195c7b11b7d8dd
Elyse Montano <elysemontano.dev.com>
2023-06-30 14:35:31 -0700

    adds fetch calls for read and create

--- README.md
+++ README.md
@@ -9,3 +9,22 @@ Read:
 - Edit
 - New
 
+# Fetch
+- Fetch - makes asynchronous requests
+  - Javascript is single threaded - only does one thing at a time
+  - Fetch allows us to multitask
+  - Fetch sends a request and waits for a response
+  - Fetch returns a promise
+    - Promise has 3 states:
+      - pending
+      - fulfilled
+        - gets payload as a result
+      - rejected
+        - gets error
+
+# Setup to run both servers
+- Start Rails server first
+- Start React server second
+  - type 'y' to open on new port
+
+  
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {React, useState} from 'react'
+import {React, useState, useEffect} from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -12,11 +12,40 @@ import mockCats from './mockCats'
 import "./App.css"
 
 const App = () => {
-  const [cats, setCats] = useState(mockCats)
+  const [cats, setCats] = useState([])
+
+  useEffect(() => {
+    readCat()
+  }, [])
+
+  const readCat = () => {
+    // fetch is calling on the endpoint url we wish to communicate to (default is a GET request)
+    fetch("http://localhost:3000/cats")
+    // using .then to check the promise that fetch gives.  We then need to parse to json so that our payload is available in the next step
+    .then((response) => response.json())
+    // Once we get the response parsed we can update our state variable to the payload
+    .then((payload) => setCats(payload))
+    // If promise is rejected we want to catch and console log the error
+    .catch((errors) => console.log("Cat read errors:", errors))
+  }
+
+
 
   const createCat = (cat) => {
-    console.log("Created cat", cat)
+    fetch("http://localhost:3000/cats", {
+      // convert object to string that can be passed in the request
+      body: JSON.stringify(cat),
+      // specify info is sent in JSON and should be returned as JSON
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST"
+    })
+    .then((response) => response.json())
+    .then(() => readCat())
+    .catch((errors) => console.log("Cat create errors:", errors))
   }
+
 
   return (
     <>
